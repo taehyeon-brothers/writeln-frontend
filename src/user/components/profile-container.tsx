a42ef1e83be3a4ba51732668973509ec8847de8a
Byeolchan Kim <qufstar46@gmail.com>
2025-04-20 11:43:17 +0900

    feat: make profile container sever component

--- src/user/components/profile-container.tsx
+++ src/user/components/profile-container.tsx
@@ -1,47 +1,24 @@
-import { useCallback, useEffect, useState } from "react";
 import { ProfileForm } from "./profile-form";
 import { getCurrentUserProfile, updateUserProfile } from "@/src/user/apis";
-import type { UserResponse } from "@/src/user/apis/types";
-import { toast } from "sonner";
+import { cookies } from "next/headers";
 
-export function ProfileContainer() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [profileData, setProfileData] = useState<UserResponse | null>(null);
+export async function ProfileContainer() {
+  const cookieStore = cookies();
+  const accessToken = await cookieStore.get("accessToken")?.value;
+  console.log("accessToken", accessToken);
+  const profileData = await getCurrentUserProfile();
 
-  useEffect(() => {
-    fetchProfile();
-  }, []);
-
-  const fetchProfile = async () => {
+  const handleSave = async (data: {
+    nickname: string;
+    introduction: string;
+  }) => {
+    "use server";
     try {
-      setIsLoading(true);
-      const data = await getCurrentUserProfile();
-      setProfileData(data);
+      await updateUserProfile(data);
     } catch (error) {
-      toast.error("프로필을 불러오는 중 오류가 발생했습니다.");
-    } finally {
-      setIsLoading(false);
+      console.log(error);
     }
   };
-
-  const handleSave = useCallback(
-    async (data: { nickname: string; introduction: string }) => {
-      try {
-        setIsLoading(true);
-        await updateUserProfile(data);
-        toast.success("프로필이 저장되었습니다.");
-      } catch (error) {
-        toast.error("프로필 저장 중 오류가 발생했습니다.");
-      } finally {
-        setIsLoading(false);
-      }
-    },
-    []
-  );
-
-  if (isLoading && !profileData) {
-    return <div>로딩 중...</div>;
-  }
 
   return (
     <ProfileForm
--- src/user/pages/profile-edit-page.tsx
+++ src/user/pages/profile-edit-page.tsx
@@ -1,22 +1,15 @@
-"use client";
-
-import { useState } from "react";
 import Header from "@/src/base/components/header";
-import Footer from "@/src/base/components/footer";
+import FooterContainer from "@/src/base/components/footer-container";
 import { ProfileContainer } from "../components/profile-container";
 
 export default function ProfileEditPage() {
-  const [activeTab, setActiveTab] = useState<"home" | "messages" | "profile">(
-    "profile"
-  );
-
   return (
-    <main className="flex flex-1 flex-col bg-rose-50">
+    <main className="flex min-h-screen flex-1 flex-col bg-rose-50">
       <Header />
       <div className="flex-1 px-4 py-4">
         <ProfileContainer />
       </div>
-      <Footer activeTab={activeTab} onTabChange={setActiveTab} />
+      <FooterContainer />
     </main>
   );
 }
