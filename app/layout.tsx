6e3b483b7609642f071bd3a82c868f17837cb0ed
Byeolchan Kim <qufstar46@gmail.com>
2025-04-20 14:10:32 +0900

    fix: unexisting className

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <ProfileGuard>{children}</ProfileGuard>
         <Toaster />
       </body>
